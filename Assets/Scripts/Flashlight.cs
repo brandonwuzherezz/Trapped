32989bfda8585f04e1b982f272bec3346e0ca480
jrparikh <30482649+jrparikh@users.noreply.github.com>
2018-02-02 14:56:51 -0800

    Revert "Flashlight now properly follows mouse."
    This reverts commit 76eec261998772ab9d021fb47c846c5e68b4d093.

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -22,18 +22,26 @@ public class Flashlight : MonoBehaviour
 
     public Text text;
 
-    public float speed = 5.0f;
+
+
+    //public Camera camera;
+    //private mousePosition;
+    //private Camera myCamera;
 
     AudioSource audioSource;
+
 
     // Use this for initialization
     void Start()
     {
+
         myLight = GetComponent<Light>();
         batteryLife = myLight.intensity;
 
         flashlightbar.value = maxIntensity;
         text = GetComponent<Text>();
+
+
         audioSource = GetComponent<AudioSource>();
 
     }
@@ -60,14 +68,38 @@ public class Flashlight : MonoBehaviour
 
             flashlightbar.value = myLight.intensity / maxIntensity;
 
-            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.y); //mouse position
+
+            // transform.rotation = myCamera.ScreenToWorldPoint(Vector3(Input.mousePosition.x, /*Input.mousePosition.y*/0, Input.mousePosition.y));
+            //transform.rotation = 
+            /*var pos = Camera.main.WorldToScreenPoint(transform.position);
+            var dir = Input.mousePosition - pos;
+            var angle = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);*/
+            //transform.Rotate(new Vector3(0, 0, 200) * speed);
+
+            //var pos = Input.mousePosition;
+            //var worldpos = Camera.main.ScreenToWorldPoint(new Vector3(pos.x, pos.y, pos.y));
+            // Debug.Log(pos);
+            //Vector3 target = new Vector3(pos.x, pos.y, pos.y);
+            /*Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.z, 10);
+            Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+            Vector3 targetDir = lookPos - transform.position;
+            //float step = speed * Time.deltaTime;
+            Vector3 newDir = lookPos - targetDir; //Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+            //Debug.Log(newDir);
+            Quaternion rotation = Quaternion.LookRotation(newDir);
+            transform.rotation = rotation;*/
+
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y); //mouse position
             //Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos); // convert to position in the world
-            //lookPos = lookPos - transform.position; // offset by the position of flashlight
-            float angle = Mathf.Atan2(mousePos.x - 477.0f, mousePos.z - 371.0f) * Mathf.Rad2Deg; // arctan b/w x and y            
-            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up); //rotate based on angle and axis(forward = z and up = y)      
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
-            //Debug.Log("lookPos: " + lookPos);
-            
+            mousePos = mousePos - transform.position; // offset by the position of flashlight
+            float angle = Mathf.Atan2(mousePos.x, mousePos.z) * Mathf.Rad2Deg; // arctan b/w x and y
+            transform.localRotation = Quaternion.AngleAxis(angle, Vector3.up); //rotate based on angle and axis(forward = z and up = y)
+            //transform.rotation = rotation;
+            Debug.Log(mousePos);
+            //mousePosition = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - camera.transform.position.z));
+            //rigidbody.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((mousePosition.y - transform.position.y), (mousePosition.x - transform.position.x)) * Mathf.Rad2Deg - 90);
+
             if (myLight.intensity <= 0)
             {
                 myLight.enabled = false;
